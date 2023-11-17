e22f2abcc8f1c63ff77c7964783185142998a0b5
RoopSubudhi <140480801+RoopSubudhi@users.noreply.github.com>
2023-11-17 11:19:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express');
-const router = express.Router();
+const expressLayout = require('express-ejs-layouts');
+const env = require('./config/environment');
+const port = process.env.PORT|| 8000;
+const app = express();
+const db = require(env.db_path);
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
 const passport = require('passport');
-const employee = require('../controller/employee');
-router.get('/', employee.SignInPage);
-router.post('/sign_in', passport.authenticate('local', { failureRedirect: '/' }), employee.SignIn);
-router.get('/signUp', employee.createSessionPage);
-router.get('/destroy_session' , employee.SignOut);
-router.post('/create_session', employee.createSession);
-router.use('/employee', require('./employeedashboard'));
-router.use('/student', require('./interview'));
-router.use('/result', require('./result'));
-router.use('/job', require('./job'));
-module.exports = router; 
+const passportLocal = require(env.passport_path);
+const customMware = require(env.customMware_path);
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(expressLayout);
+app.use(express.static(env.assets_path));
+app.use(session({
+    name: 'placementCell',
+    secret: env.secret_key,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store:mongoStore.create({
+        mongoUrl :env.mongoose_path,
+        ttl: 14 * 24 * 60 * 60
+    })
+    }
+
+));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(customMware.setFlash);
+app.use('/', require('./route/index'));
+app.listen(port, function (error) {
+    if (error) {
+        console.log("Error in running Server");
+    }
+    console.log("Server is running");
+});
